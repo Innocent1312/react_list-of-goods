085ac5171070bc38fc15e069a20d502a26cb2fdd
Innocent1312 <anthonydev33@gmail.com>
2019-12-09 14:24:15 +0200

    Updates

---
+++ precache-manifest.518f2bbda39294ea7abd0e63f8ef2689.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a0ddf2db2a576dde65bd49d13c44f51",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "5a24a4d87377b5283f3c",
+    "url": "/react_list-of-goods/static/js/2.4a80f5eb.chunk.js"
+  },
+  {
+    "revision": "42ec2ecec5c2f52d352a",
+    "url": "/react_list-of-goods/static/js/main.4132f7f8.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
